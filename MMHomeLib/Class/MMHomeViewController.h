d8cb4d47aa641a125f59187ef11078e772f38c81
Loren <hyl946@163.com>
2018-04-21 02:47:57 +0800

    homeViewController

---
+++ MMHomeLib/Class/MMHomeViewController.h
@@ -0,0 +1,14 @@
+//
+//  MMHomeViewController.h
+//  MMHome
+//
+//  Created by Loren on 2018/4/21.
+//  Copyright © 2018年 Loren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MMBaseViewController.h>
+
+@interface MMHomeViewController : MMBaseViewController
+
+@end
---
+++ MMHomeLib/Class/MMHomeViewController.m
@@ -0,0 +1,18 @@
+//
+//  MMHomeViewController.m
+//  MMHome
+//
+//  Created by Loren on 2018/4/21.
+//  Copyright © 2018年 Loren. All rights reserved.
+//
+
+#import "MMHomeViewController.h"
+
+@implementation MMHomeViewController
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+
+    [self setTitle:@"HomePage" leftTitle:@"back" rightTitle:@"push"];
+}
+@end
